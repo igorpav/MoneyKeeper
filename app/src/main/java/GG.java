b207f0c6b0fa827d6463fb1f0eaa088181403747
igorpav <ipavlichenkov@gmail.com>
2018-07-11 19:02:05 +0300

    add new fragments

--- app/src/main/java/GG.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by igorpav on 11.07.18.
- */
-
-public class GG {
-    //Aliya super class)))
-    //test pull
-}
---
+++ app/src/main/java/team/e/com/moneykeeper/ReportFragment.java
@@ -0,0 +1,8 @@
+package team.e.com.moneykeeper;
+
+/**
+ * Created by igorpav on 11.07.18.
+ */
+
+public class ReportFragment {
+}
