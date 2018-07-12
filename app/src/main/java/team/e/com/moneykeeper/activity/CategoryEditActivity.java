eeb995ffdf77c8f146801a2123af686bac974a50
Aliya Badygova <73aliysha73@mail.ru>
2018-07-12 11:52:33 +0300

    add category edit and expense edit activities

---
+++ app/src/main/java/team/e/com/moneykeeper/activity/CategoryEditActivity.java
@@ -0,0 +1,33 @@
+package team.e.com.moneykeeper.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+
+import import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+
+import team.e.com.moneykeeper.fragment.CategoryEditFragment;
+
+public class CategoryEditActivity extends BaseFragmentActivity {
+
+    /* Important: use onCreate(Bundle savedInstanceState)
+     * instead of onCreate(Bundle savedInstanceState, PersistableBundle persistentState) */
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        insertFragment(new CategoryEditFragment());
+        setupActionBar();
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar (toolbar).
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
+
