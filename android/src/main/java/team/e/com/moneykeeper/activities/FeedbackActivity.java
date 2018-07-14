4c58b98e18158239102e143292bd8b5256a43be6
KornilovMikhail <31703085+KornilovMikhail@users.noreply.github.com>
2018-07-14 10:42:43 +0300

    feedback is fully operational

--- android/src/main/java/team/e/com/moneykeeper/activities/FeedbackActivity.java
+++ android/src/main/java/team/e/com/moneykeeper/activities/FeedbackActivity.java
@@ -1,4 +1,40 @@
 package team.e.com.moneykeeper.activities;
 
-class FeedbackActivity {
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.view.View;
+import android.widget.EditText;
+
+import team.e.com.moneykeeper.R;
+import team.e.com.moneykeeper.fragments.FeedbackFragment;
+
+
+public class FeedbackActivity extends BaseFragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        insertFragment(new FeedbackFragment());
+        setupActionBar();
+    }
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar (toolbar).
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+    public void onSendMessage(View view) {
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        String uriText = "mailto:" + Uri.encode("kmb99@mail.ru") +
+                "?subject=" + Uri.encode("Feedback") +
+                "&body=" + Uri.encode(messageText);
+        Uri uri = Uri.parse(uriText);
+        intent.setData(uri);
+        startActivity(Intent.createChooser(intent,"Отправить через..."));
+    }
 }
---
+++ android/src/main/java/team/e/com/moneykeeper/fragments/FeedbackFragment.java
@@ -0,0 +1,23 @@
+package team.e.com.moneykeeper.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import team.e.com.moneykeeper.R;
+
+public class FeedbackFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_feedback,container,false);
+    }
+
+}
